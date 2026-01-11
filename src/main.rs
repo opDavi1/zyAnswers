d641be15b08eed7a205cbfb41fe96aed88f66326
opDavi1 <121784309+opDavi1@users.noreply.github.com>
2026-01-10 23:29:51 -0800

    feat: add very verbose command line option
    fix: update cli

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ mod cli;
 mod zybooks;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = cli::Cli::parse();
+    let mut args = cli::Cli::parse();
+    if args.very_verbose {
+        args.verbose = true;
+    }
     let data = zybooks::get_zybooks_data(&args)?;
 
     let parser = zybooks::QuestionParser::new(data);
